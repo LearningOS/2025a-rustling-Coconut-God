23dc99a20a42ce778af0913b2e2e5c4f9f81ca37
YourName <your-email@example.com>
2025-09-29 15:50:15 +0800

    update rustlings exerscises

--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint threads2` or use the `hint` watch subcommand for a
 // hint.
 
-//没做
+
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
@@ -17,24 +17,20 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });//arc可以让多个线程共享所有权
-    let mut handles = vec![];
-    for _ in 0..10 {
-        let status_shared = Arc::clone(&status);
-        let handle = thread::spawn(move || {
-            thread::sleep(Duration::from_millis(250));
-                let mut status = status_shared.lock().unwrap();
-            // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
-        });
-        handles.push(handle);
-    }
-    for handle in handles {
-        handle.join().unwrap();
-        // TODO: Print the value of the JobStatus.jobs_completed. Did you notice
-        // anything interesting in the output? Do you have to 'join' on all the
-        // handles?
-         let completed = status.lock().unwrap().jobs_completed;
-        println!("jobs completed {}", completed);
-    }
+   let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
+let mut handles = vec![];
+for _ in 0..10 {
+    let status_shared = Arc::clone(&status);
+    let handle = thread::spawn(move || {
+        thread::sleep(Duration::from_millis(250));
+        let mut status = status_shared.lock().unwrap();
+        status.jobs_completed += 1;
+    });
+    handles.push(handle);
 }
+for handle in handles {
+    handle.join().unwrap();
+}
+let completed = status.lock().unwrap().jobs_completed;
+println!("jobs completed {}", completed);
+    }
