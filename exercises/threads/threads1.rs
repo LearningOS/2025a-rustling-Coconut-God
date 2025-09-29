42c7fcf41de36a2397fb94f80d2e521b6db567e3
YourName <your-email@example.com>
2025-09-29 17:58:22 +0800

    update rustlings exercises

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -10,6 +10,13 @@
 
 
 
+// threads1.rs
+//
+// This program spawns multiple threads that each run for at least 250ms, and
+// each thread returns how much time they took to complete. The program should
+// wait until all the spawned threads have finished and should collect their
+// return values into a vector.
+
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -26,7 +33,11 @@ fn main() {
 
     let mut results: Vec<u128> = vec![];
     for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
+       
+        match handle.join() {
+            Ok(result) => results.push(result),
+            Err(e) => panic!("Thread panicked: {:?}", e),
+        }
     }
 
     if results.len() != 10 {
